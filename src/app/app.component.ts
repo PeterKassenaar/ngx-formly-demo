49a3f22ef19130034888f98f04a59388a880f6af
PeterKassenaar <info@kassenaar.com>
2018-06-01 16:13:43 +0200

    Added first form and model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  form = new FormGroup({});
+  model = {
+    email: '',
+    name: '',
+    age: '',
+    newsletter: false
+  };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      className: 'input-lg',
+      templateOptions: {
+        type: 'email',
+        label: 'Email address',
+        placeholder: 'Your email',
+        required: true
+      }
+    },
+    {
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Your name',
+        placeholder: 'Your name',
+        required: true
+      }
+    },
+    {
+      key: 'age',
+      type: 'input',
+      templateOptions: {
+        type: 'number',
+        label: 'Your age',
+        placeholder: 'age',
+        min: 12,
+        max: 99
+      }
+    },
+    {
+      key: 'newsletter',
+      type: 'input',
+      templateOptions: {
+        type: 'checkbox',
+        label: 'Do you want to receive our newsletter?'
+      }
+    }
+  ];
+
+  submit(model) {
+    console.log(model);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
